aa3be6bc4379b52578daefead07315b6df9c17c8
张振虎 <zhangzhenhu1@100tal.com>
2020-05-20 11:14:34 +0800

    dev

--- zzh/mllib/evaluation/evaluation.py
+++ zzh/mllib/evaluation/evaluation.py
@@ -17,6 +17,18 @@ from math import ceil
 from IPython.display import display
 from zzh.mllib.feature.dataset import DataSet
 
+params = {'font.family': 'serif',
+          'font.serif': 'Times New Roman',
+          'font.style': 'italic',
+          'font.weight': 'normal',  # or 'blod'
+          'font.size': 'medium',  # or large,small
+          'axes.unicode_minus': False
+          }
+plt.rcParams.update(params)
+# plt.rcParams['font.family'] = ['sans-serif']  # 用来正常显示中文标签
+# plt.rcParams['font.sans-serif'] = ['Tahoma']  # 用来正常显示中文标签
+# plt.rcParams['axes.unicode_minus'] = False  # 用来正常显示负号
+
 
 def gini(actual, pred):
     assert (len(actual) == len(pred))
--- zzh/mllib/model/tree.py
+++ zzh/mllib/model/tree.py
@@ -24,8 +24,6 @@ import os
 from zzh.mllib.model import ABCModel
 from zzh.mllib.feature import DataSet
 
-# plt.rcParams['font.sans-serif'] = ['SimHei']  # 用来正常显示中文标签
-plt.rcParams['axes.unicode_minus'] = False  # 用来正常显示负号
 
 
 class Xgboost(ABCModel):
